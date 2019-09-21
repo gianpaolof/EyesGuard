5a218f33dbc93f3f117ab8f05d3a3d6f217bf9dc
gianpaolof <gianpaolof@mail.com>
2019-09-21 21:55:37 +0200

    Window is not close anymore, so that only one instance can always be used

--- Source/EyesGuard/Views/Windows/BreakWindow.cs
+++ Source/EyesGuard/Views/Windows/BreakWindow.cs
@@ -1,33 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EyesGuard.Views.Animations;
 using System.Windows;
-using EyesGuard.Views.Animations;
 
 namespace EyesGuard.Views.Windows
 {
     public class BreakWindow : Window
     {
  
-        public async void HideWindow()
-        {
-            await (this as Window).HideUsingLinearAnimationAsync();
-            base.Close();
-        }
+        public async void HideWindow() => await (this as Window).HideUsingLinearAnimationAsync();
 
         public async void ShowWindow()
         {
-            try
-            {
                 await (this as Window).ShowUsingLinearAnimationAsync();
                 Show();
                 BringIntoView();
                 Focus();
-            }
-            catch { }
-
         }
     }
 }
